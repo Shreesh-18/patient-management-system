21842d92ba916bc5e3dff390a154d4eba63369f5
Your NameShreesh-18 <you@example.comshreeshjog@gmail.com>
2025-09-10 23:24:16 +0530

    updated typo

--- infrastructure/localstack-deploy.sh
+++ infrastructure/localstack-deploy.sh
@@ -7,4 +7,4 @@ aws --endpoint-url=http://localhost:4566 cloudformation deploy \
     --template-file "./cdk.out/localstack.template.json"
 
 aws --endpoint-url=http://localhost:4566 elbv2 describe-load-balancers \
-    --query "LoadBalancer[0].DNSName" --output text
+    --query "LoadBalancers[0].DNSName" --output text
